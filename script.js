eb58175b366499ab74718adc8afcd43c2a316a48
yaswanthiyashu <107607008+yaswanthiyashu@users.noreply.github.com>
2023-05-02 19:41:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let OnBulb = document.getElementById('on-bulb').src;
+let OffBulb = document.getElementById('off-bulb').src;
+
+
+function swap() {
+    document.getElementById('off-bulb').src = OnBulb;
+    document.getElementById('on-bulb').src = OffBulb;
+    
+    
+    
+}
